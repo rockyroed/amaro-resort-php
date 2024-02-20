3bbfbd12c46ab5608e46e7ff30d572b59b283519
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2024-02-20 23:40:47 +0800

    Update reservation UI #1

--- src/javascript/reservation.js
+++ src/javascript/reservation.js
@@ -8,11 +8,14 @@ const rightBook = document.getElementById('right-book');
 
 for(let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
-        rightBook.classList.remove('background-visible');
 
     // Toggle visibility of guest details container
     document.querySelector('.right-container').classList.add('hidden');
     document.querySelector('.form-container').classList.remove('hidden');
+    document.querySelector('.right-book').style.background = 'none';
+    document.querySelector('.left-book').style.background = 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.60)) center/cover no-repeat, url("../css/page-images/1Swimming.png") center/cover no-repeat';
+    document.querySelector('.left-book').style.color = 'white';
+
 
         const index = i;
         for(let i = 0; i < buttons.length; i++) {
